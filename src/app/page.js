ab5220d1ec7a58200fa6229127dcf07c08044b5b
Maysam-Ghaysari <maysamghaysari80@gmail.com>
2025-12-10 13:29:16 +0330

    Update images and code

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Navbar />
       <HeroAbout />
       <WhatIDo />
-      <RoadmapPage/>
+      <RoadmapPage />
       <ProjectsSection />
       <SkillsSection />
       <About />
