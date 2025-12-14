545afbc3985d3299d221fec0490d15d052e7a2d6
Maysam-Ghaysari <maysamghaysari80@gmail.com>
2025-12-14 15:57:22 +0330

    Update images and code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -54,7 +54,7 @@ export default function ProjectsSection() {
       id: 6,
       title: "Wealzi",
       description:
-        "At Walzi, we have taken a new step in providing investment and portfolio management services by creating a secure platform for digital asset investment. We consider it our mission to guide all stages of investment with intelligence and simplicity, enhancing investment capabilities for people from all walks of life.",
+        "At Wealzi, we have taken a new step in providing investment and portfolio management services by creating a secure platform for digital asset investment. We consider it our mission to guide all stages of investment with intelligence and simplicity, enhancing investment capabilities for people from all walks of life.",
       image: "/projects/wealzi.png",
       tags: ["Next.js ", "Tailwind", "Responsive"],
       demo: "https://polite-toffee-7510f3.netlify.app",
